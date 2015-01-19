45b700d6f7f0c20397eb6fbe4d4f7004d2ce7071
François LEPORCQ <fleporcq@gmail.com>
2015-01-19 13:58:51 +0100

    Ajout de la dépendence sluggable

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+		'Cviebrock\EloquentSluggable\SluggableServiceProvider',
 
 	),
 
